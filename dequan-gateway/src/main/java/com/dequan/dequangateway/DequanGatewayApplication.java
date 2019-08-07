d5f5d16aa51210b1ddc623d6be6c3e793cd165aa
lipeng <1151449905@qq.com>
2019-08-08 00:36:07 +0800

    新增项目

---
+++ dequan-gateway/src/main/java/com/dequan/dequangateway/DequanGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dequan.dequangateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DequanGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DequanGatewayApplication.class, args);
+    }
+
+}
---
+++ dequan-pay/src/test/java/com/dequan/dequanpay/DequanPayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dequan.dequanpay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DequanPayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
