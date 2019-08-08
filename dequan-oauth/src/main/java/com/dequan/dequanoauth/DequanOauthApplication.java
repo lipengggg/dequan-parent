d239a5b27cfa9d2e0028deb995e31e72537e061b
lipeng <1151449905@qq.com>
2019-08-08 23:28:04 +0800

    新增项目

---
+++ dequan-oauth/src/main/java/com/dequan/dequanoauth/DequanOauthApplication.java
@@ -0,0 +1,13 @@
+package com.dequan.dequanoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DequanOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DequanOauthApplication.class, args);
+    }
+
+}
